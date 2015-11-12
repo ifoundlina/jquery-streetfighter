f0131a04ebb7150c17c223fcc26076a7669446ed
“Lina <ifoundlina@gmail.com>
2015-11-11 21:20:00 -0500

    last update WED

--- js/app.js
+++ js/app.js
@@ -1,33 +1,57 @@
-$(document).ready(function(){
-    $('.ryu').mouseenter(function(){
-        $('.ryu-still').hide();
-        $('.ryu-ready').show();
-    })
-    .mouseleave(function() {
-        $('.ryu-still').show();
-        $('.ryu-ready').hide();
-    })
-    .mousedown(function(){
-        playHadouken();
-        $('.ryu-ready').hide();
-        $('.ryu-throwing').show();
-        $('.hadouken').finish().show().animate(
-            {'left': '1020px'},
-            500,
-            function(){
-                $(this).hide();
-                $(this).css('left', '520px');
-            });
-    })
-    .mouseup(function(){
-        $('.ryu-throwing').hide();
-        $('.ryu-ready').show();
-    });
-
+$(document).ready(function() {
+	$('html').keydown(function(event) {
+		if(event.which == 88) {
+			$('.ryu-ready').hide();
+			$('.ryu-throwing').hide();
+			$('.ryu-still').hide();
+			$('.ryu-cool').show();
+		}
+	})
+	.keyup(function(event) {
+		if(event.which == 88) {
+			$('.ryu-ready').hide();
+			$('.ryu-throwing').hide();
+			$('.ryu-cool').hide();
+			$('.ryu-still').show();
+		}
+	});
+	
+	$('.ryu').mouseenter(function() {
+		if($('.ryu-cool').is(":hidden")) {
+			$('.ryu-still').hide();
+			$('.ryu-throwing').hide();
+			$('.ryu-ready').show();
+		}
+	})
+	.mouseleave(function() {
+		if($('.ryu-cool').is(":hidden")) {
+			$('.ryu-ready').hide();
+			$('.ryu-still').show();
+		}
+	})
+	.mousedown(function() {
+		if($('.ryu-cool').is(":hidden")) {
+			playHadouken();
+			$('.ryu-ready').hide();
+			$('.ryu-still').hide();
+			$('.ryu-throwing').show();
+			$('.hadouken').finish().show()
+			.animate({'left': '400px'}, 500, function() {
+            	$(this).hide();
+				$(this).css('left', '-212px');
+			});
+		}
+	})
+	.mouseup(function() {
+		if($('.ryu-cool').is(':hidden')) {
+			$('.ryu-throwing').hide();
+			$('.ryu-ready').show();
+		}
+	});
 });
 
 function playHadouken() {
-        $('#hadouken-sound')[0].volume = 0.5;
-        $('#hadouken-sound')[0].load();
-        $('#hadouken-sound')[0].play();
-    }
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
